38a5e36f67d0d2877642d53de4b1700e4502e870
oxdjww <oxdjww@gmail.com>
2023-06-28 01:39:45 +0900

    Create spring starter template

---
+++ src/main/java/com/flag/flag_back/FlagBackApplication.java
@@ -0,0 +1,13 @@
+package com.flag.flag_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlagBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flag/flag_back/FlagBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flag.flag_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlagBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
