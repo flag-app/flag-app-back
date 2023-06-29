fa4ab0f18c0414ab5a1641e1cdbfa7191a786a7a
oxdjww <oxdjww@gmail.com>
2023-06-29 21:49:10 +0900

    Connect MySQL DB

--- src/test/java/com/flag/flag_back/FlagBackApplicationTests.java
+++ src/test/java/com/flag/flag_back/FlagBackApplicationTests.java
@@ -2,12 +2,10 @@ package com.flag.flag_back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class FlagBackApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
