865b08bbc13bcefee17719eb5d97ffbf81f6ead4
Eunkyy <melon0802@naver.com>
2023-07-21 15:04:25 +0900

    회원가입 조건 수정
    .

--- src/main/java/com/flag/flag_back/Dto/UserInfo.java
+++ src/main/java/com/flag/flag_back/Dto/UserInfo.java
@@ -3,13 +3,22 @@ package com.flag.flag_back.Dto;
 import com.flag.flag_back.Model.User;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
 public class UserInfo {
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,7}$", message = "닉네임은 특수문자를 제외한 2~7자리여야 합니다.")
     private String name;
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
 
     public User toEntity() {
