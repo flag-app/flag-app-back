397ae9071334d0e6f1f665774914a6d7d99fdeb9
JungTae Kwon <oxdjww@gmail.com>
2023-08-15 05:27:45 +0900

    Add testcase

--- src/test/java/com/flag/flag_back/FlagBackApplicationTests.java
+++ src/test/java/com/flag/flag_back/FlagBackApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FlagBackApplicationTests {
-
+	@Test
+	void contextLoads() {
+	}
 	
 }
