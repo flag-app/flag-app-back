1c83dd1dfd49f04c3076ae06aed7f841ec1e79a5
Chan531 <chan657312@gmail.com>
2023-08-21 04:28:32 +0900

    친구 컨트롤러 토큰 적용

--- src/main/java/com/flag/flag_back/Controller/FriendController.java
+++ src/main/java/com/flag/flag_back/Controller/FriendController.java
@@ -6,6 +6,8 @@ import com.flag.flag_back.Dto.FriendRes;
 import com.flag.flag_back.Dto.UserResponse;
 import com.flag.flag_back.Model.Friend;
 import com.flag.flag_back.Model.User;
+import com.flag.flag_back.Repository.UserRepository;
+import com.flag.flag_back.jwt.JwtTokenProvider;
 import com.flag.flag_back.service.FriendService;
 import com.flag.flag_back.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -22,16 +24,18 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("friends")
 public class FriendController {
-    @Autowired
-    private final FriendService friendService;
 
-    @Autowired
+    private final FriendService friendService;
     private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
 
     @GetMapping("/List/{name}") //닉네임으로 리스트 조회
     @Operation(summary = "닉네임 검색", description = "닉네임으로 유저 검색")
-    public List<UserResponse> getUsersList(@PathVariable("name") String name) {
+    public List<UserResponse> getUsersList(@RequestHeader(value = "Authorization", required = false) String token, @PathVariable("name") String name) {
         try {
+            String email = jwtTokenProvider.getUserPk(token);
+            userRepository.findUserByEmail(email);
             return userService.findListByName(name);
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -39,6 +43,18 @@ public class FriendController {
     }
 
     @PostMapping("/add")
+    @Operation(summary = "친구 추가", description = "닉네임으로 친구 추가")
+    public String addFriend(@RequestHeader(value = "Authorization", required = false) String token, @RequestBody @Valid String friendName) {
+        try {
+            String email = jwtTokenProvider.getUserPk(token);
+            User user = userRepository.findUserByEmail(email);
+            User friend = userRepository.findUserByName(friendName);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*@PostMapping("/add")
     @Operation(summary = "친구 추가", description = "id로 유저 친구 추가")
     public FriendRes create(@RequestBody @Valid FriendDto dto) {
         if(checkUser(dto.getUserId(), dto.getUserId2()) == true){
@@ -50,14 +66,17 @@ public class FriendController {
 
         Long id = friendService.add(friend);
         return new FriendRes(id);
-    }
+    }*/
 
     //친구인지 아닌지 검사
-    @GetMapping("/checkFriend/{id}/{id2}") //닉네임으로 리스트 조회
+    @GetMapping("/checkFriend") //닉네임으로 리스트 조회
     @Operation(summary = "친구 중복 검사", description = "친구인지 아닌지 검사")
-    public boolean checkUser(@PathVariable("id") Long id, @PathVariable("id2") Long id2) {
+    public boolean checkUser(@RequestHeader(value = "Authorization", required = false) String token, @RequestBody @Valid String friendName) {
         try {
-            return friendService.checkFriendById2(id, id2);
+            String email = jwtTokenProvider.getUserPk(token);
+            User user = userRepository.findUserByEmail(email);
+
+            return friendService.checkFriendById2(0L, 0L);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -66,8 +85,10 @@ public class FriendController {
     //친구 리스트 보여줌.
     @GetMapping("/friendList/{id}")
     @Operation(summary = "친구 list 조회", description = "내 친구 목록 조회.")
-    public List<UserResponse> getFriendsList(@PathVariable("id") Long id) {
+    public List<UserResponse> getFriendsList(@RequestHeader(value = "Authorization", required = false) String token, @PathVariable("id") Long id) {
         try {
+            String email = jwtTokenProvider.getUserPk(token);
+            User user = userRepository.findUserByEmail(email);
             return friendService.friendsListById(id);
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -77,8 +98,10 @@ public class FriendController {
     //내 친구 내에서 검색 - select문을 - where userid = user2Id where userid1 =  myId
     @GetMapping("/friendList/{id}/{name}") //닉네임으로 리스트 조회
     @Operation(summary = "친구 내에서 닉네임 검색", description = "내 친구 리스트에서 닉네임으로 친구 검색")
-    public List<UserResponse> searchFriendsList(@PathVariable("id") Long id, @PathVariable("name") String name) {
+    public List<UserResponse> searchFriendsList(@RequestHeader(value = "Authorization", required = false) String token, @PathVariable("id") Long id, @PathVariable("name") String name) {
         try {
+            String email = jwtTokenProvider.getUserPk(token);
+            User user = userRepository.findUserByEmail(email);
             return friendService.friendsListByNickName(id, name);
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -88,7 +111,10 @@ public class FriendController {
     //친구 삭제
     @DeleteMapping("/delete/{id}/{fid}")
     @Operation(summary = "친구 삭제", description = "친구 삭제 API")
-    public Map<String, Object> delete(@PathVariable("id") Long id, @PathVariable("fid") Long fid) {
+    public Map<String, Object> delete(@RequestHeader(value = "Authorization", required = false) String token, @PathVariable("id") Long id, @PathVariable("fid") Long fid) {
+
+        String email = jwtTokenProvider.getUserPk(token);
+        User user = userRepository.findUserByEmail(email);
         Map<String, Object> response = new HashMap<>();
 
         if(friendService.delete(id,fid) > 0) {
